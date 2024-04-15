1f6ef14e97a1659e022b2d9d7d09811c156cb810
Lucía Castro <90776369+lucastro95@users.noreply.github.com>
2024-04-15 16:52:20 -0300

    agregan assets y config de i18n

---
+++ app/assets/strings/l18n.js
@@ -0,0 +1,13 @@
+import {I18n as i18n} from "i18n-js"
+import * as RNLocalize from "react-native-localize"
+import en from "./translations/en"
+
+const locales = RNLocalize.getLocales()
+
+translations = {en}
+var I18n = new i18n(translations)
+
+I18n.locale = locales[0].languageCode
+
+I18n.fallbacks = true
+export default I18n
