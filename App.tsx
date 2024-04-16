32a3e2427456ed33ea2da0359aaf8156367a5ede
Lucía Castro <90776369+lucastro95@users.noreply.github.com>
2024-04-16 15:12:28 -0300

    splash screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   Text,
   View,
 } from 'react-native';
 import I18n from './app/assets/strings/l18n';
+import SplashScreen from 'react-native-splash-screen';
 
 function App(): React.JSX.Element {
+  
+  useEffect(() => {
+    SplashScreen.hide();
+  }, [])
+
   return (
     <View>
       <Text>{I18n.t('hola')}</Text>
