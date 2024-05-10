4661e5b0bdf17ba8ca71bc7860e5823dafc22937
Lucía Castro <90776369+lucastro95@users.noreply.github.com>
2024-05-09 21:07:35 -0300

    logo en header

--- app/ui/components/home/Header.jsx
+++ app/ui/components/home/Header.jsx
@@ -1,28 +1,37 @@
-import { Image, StyleSheet, Text, View } from "react-native"
-import Logo from '../../../assets/images/logo.svg'
-import { colors } from "../../styles/RootColors"
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import { colors } from '../../styles/RootColors';
+import Logo from '../../../assets/images/logo.png'
 
 const Header = () => {
   return (
-    <View styles={styles.container}>
-      <Image source={Logo} style={styles.logo}/>
-      <Text>Hola</Text>
+    <View style={styles.container}>
+      <LinearGradient 
+        locations={[0, 1]}
+        colors={[colors.blue, `${colors.blue}00`]}
+        style={styles.linearGradient}>
+        <Image source={Logo} style={styles.logo}/>
+      </LinearGradient>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        height: 700,
-        flex: 1,
-        justifyContent: "center",
-        background: `linear-gradient(to bottom, ${colors.pink}, rgba(0,0,0,1))`,
-    },
-    logo: {
-      width: 80,
-      objectFit: "cover"
-    }
-})
+  container: {
+    width: '100%',
+    height: '30%', 
+    justifyContent: 'center', 
+  },
+  linearGradient: {
+    flex: 1, 
+    justifyContent: 'center', 
+    paddingHorizontal: 20, 
+  },
+  logo: {
+    width: '100%', 
+    resizeMode: 'contain'
+  }
+});
 
-export default Header
+export default Header;
