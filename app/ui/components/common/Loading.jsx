ae8f1ad25c83daeec6d301ee66400de3c299c95f
Lucía Castro <90776369+lucastro95@users.noreply.github.com>
2024-06-13 11:04:35 -0300

    loader nuevo

--- app/ui/components/common/Loading.jsx
+++ app/ui/components/common/Loading.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
 import { colors } from '../../styles/RootColors';
-import LoaderKit from 'react-native-loader-kit'
 
 const Loading = () => {
     return (
         <View style={styles.background}>
-            <LoaderKit
-                style={{ width: 70, height: 70 }}
-                name={'BallSpinFadeLoader'}
-                color={`${colors.white}`} 
-            />
+            <ActivityIndicator size={100} color={colors.white} />
         </View>
     );
 };
@@ -26,20 +21,6 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         zIndex: 9999,
-    },
-    loader: {
-        width: 22,
-        height: 22,
-        borderRadius: 50,
-        backgroundColor: '#25b09b',
-        shadowColor: '#25b09b',
-        shadowOffset: {
-            width: 0,
-            height: 0,
-        },
-        shadowOpacity: 1,
-        shadowRadius: 1,
-        elevation: 10, // For Android
     },
 });
 
